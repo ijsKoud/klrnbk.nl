77cd096ce8bc572a223738f818af5dbeeabd69f5
Daan <daan@klrnbk.nl>
2025-02-25 14:23:11 +0100

    chore: initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,21 @@
+const prettierConfig = {
+	endOfLine: "auto",
+	printWidth: 150,
+	quoteProps: "as-needed",
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "none",
+	useTabs: true,
+	overrides: [
+		{
+			files: "*.yml",
+			options: {
+				tabWidth: 2,
+				useTabs: false
+			}
+		}
+	]
+};
+
+export default prettierConfig;
