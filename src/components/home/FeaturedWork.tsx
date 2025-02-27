7061f7e32e84df44b181e7cb9d9268f1f5ff3104
Daan <daan@klrnbk.nl>
2025-02-27 02:19:03 +0100

    feat: add homepage featured work section

---
+++ src/components/home/FeaturedWork.tsx
@@ -0,0 +1,31 @@
+import { GITHUB_NAME, projects } from "@/constants";
+import { FC } from "react";
+import { ProjectCard } from "./ui/project-card";
+import { Button } from "../ui/button";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+export const FeaturedWork: FC = () => {
+	return (
+		<div className="flex flex-col items-center gap-y-10">
+			<div className="flex flex-col items-center gap-y-5">
+				<h2 className="text-5xl leading-none font-bold">Featured work</h2>
+				<span className="text-base leading-normal font-normal text-muted-foreground">
+					I've worked on a variety of projects, ranging from simple websites to complex applications. Here are a few of my favorites.
+				</span>
+			</div>
+
+			<div className="flex flex-wrap items-center justify-center gap-4 w-full">
+				{projects.map((project, idx) => (
+					<ProjectCard key={idx} {...project} />
+				))}
+			</div>
+
+			<Button asChild>
+				<Link href={`https://github.com/${GITHUB_NAME}/repositories`}>
+					View all projects <ChevronRight />
+				</Link>
+			</Button>
+		</div>
+	);
+};
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+import { ProjectCardProps } from "./components/home/ui/project-card/types";
+
+export const GITHUB_NAME = "ijskoud";
+export const projects: ProjectCardProps[] = [];
