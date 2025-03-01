39a9bf6463c09f3fdb250959e03dc6952cbe8113
Daan <daan@klrnbk.nl>
2025-03-01 11:46:49 +0100

    feat(Ui): add sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const interSans = Inter({
 	variable: "--font-inter-sans",
@@ -20,6 +21,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${interSans.variable} antialiased`}>{children}</body>
+			<Toaster />
 		</html>
 	);
 }
